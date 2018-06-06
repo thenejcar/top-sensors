72ce60cf3e25e57f5e199a009df0f0ae15c0c168
Nejc Kisek <nejc@kisek.si>
2018-06-07 00:04:53 +0200

    added the optimizer to the slides, cleaned presentation dir

--- sensors.py
+++ sensors.py
@@ -55,7 +55,7 @@ def generify(points):
     r3 = random.uniform(-0.01, 0.01)
     return [(p[0] + r1, p[1] + r2, p[2] + r3) for p in points]
 
-def optimize_points(points, r, R, VR, C):
+def optimize_points(file, points, r, R, VR, C):
     opt_points, opt_vr, opt_cech = optimize_smart(points, r, R, VR, C)
 
     if opt_vr is not None and opt_cech is not None:
@@ -64,6 +64,7 @@ def optimize_points(points, r, R, VR, C):
         print("optimiser used %d/%d points" % (len(opt_points), len(points)))
         draw_earth(opt_points, "optimized " + file, R=R)
         draw_earth(opt_points, "optimized " + file, edges=opt_vr[1])
+        save_points("data/optimized_" + file + ".txt", opt_points)
     else:
         print("No redundant sensors detected")
 
@@ -81,14 +82,13 @@ def plot_barcodes():
 
 if __name__ == "__main__":
 
-    for file in ["sensors01.txt", "sensors02.txt", "generated01.txt"]:
+    for file in ["sensors01", "sensors02", "generated01"]:
         print()
         print("*****************************************************")
         print("***  " + file)
         print("*****************************************************")
 
-        points = generify(load_points("data/" + file))
-        file = file.split(".")[0]
+        points = generify(load_points("data/" + file + ".txt"))
         scene = draw_earth(points, file)
         #scene.delete()
 
@@ -110,13 +110,17 @@ if __name__ == "__main__":
         #scene.delete()
         print()
 
-        # fund redundant sensors -- this takes a long time
+        # find redundant sensors -- this takes a long time
         #optimize_points(points, r, R, VR, C)
 
 
 
     # plot the barcodes
-    #plot_barcodes()
+    plot_barcodes()
 
     # show the plots (if not called, they are still saved to pdf files)
-    #show()
+    show()
+
+
+# redundant example for sensors02
+# [(0.08153072939427779, -0.0674609629388395, -0.9941234825453283), (0.1045781293942778, -0.023768662938839513, -0.9941234825453283), (0.07954352939427779, 0.06862523706116049, -0.9941234825453283), (0.037590929394277786, 0.09470543706116048, -0.9941234825453283), (-0.011729470605722212, 0.09747993706116048, -0.9941234825453283), (-0.0853248706057222, 0.03626663706116049, -0.9941234825453283), (-0.09158117060572221, -0.012733962938839511, -0.9941234825453283), (-0.07357937060572221, -0.05873546293883951, -0.9941234825453283), (-0.03572717060572222, -0.09047506293883952, -0.9941234825453283), (0.012708149394277786, -0.10018186293883952, -0.9941234825453283), (0.05986782939427778, -0.0854792629388395, -0.9941234825453283), (0.039211029394277785, 0.5632665370611605, -0.8244554825453283), (-0.2349480706057222, 0.5094575370611605, -0.8244554825453283), (-0.4497480706057222, 0.3307975370611605, -0.8244554825453283), (-0.5525980706057223, 0.07102733706116049, -0.8244554825453283), (-0.5183170706057223, -0.2062514629388395, -0.8244554825453283), (-0.3552980706057222, -0.4331504629388395, -0.8244554825453283), (-0.1034540706057222, -0.5541184629388395, -0.8244554825453283)]
